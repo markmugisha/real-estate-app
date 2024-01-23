efe99ab903bc906583879f60e51823f1436f35f5
Mark Mugisha <141035540+markmugisha@users.noreply.github.com>
2024-01-23 15:23:19 +0300

    update header and making profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,27 +1,30 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-username: {
-    type: String,
-    required: true,
-    unique: true,
-},
-email: {
-    type: String,
-    required: true,
-    unique: true,
-},
-password: {
-    type: String,
-    required: true,
-},
-avatar: {
-    type: String,
-    default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.deviantart.com%2Fthwaitesmtt%2Fart%2FKiri-Human-Version-Avatar-2-The-Way-Of-Water-950527483&psig=AOvVaw0fWg9TW4UWh7NuOCiv09qy&ust=1706024055131000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCODu74up8YMDFQAAAAAdAAAAABAR"
-},
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+  },
+  { timestamps: true }
+);
 
-}, {timestamps: true});
-
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
