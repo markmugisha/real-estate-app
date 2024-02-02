50c4c82989c072477a01b6619e5e20d03145ad97
Mark Mugisha <141035540+markmugisha@users.noreply.github.com>
2024-02-02 13:44:43 +0300

    create the listing item component and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,6 @@
-import { set } from "mongoose";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ListingItem from "./ListingItem";
 
 const Search = () => {
   const navigate = useNavigate();
@@ -19,7 +19,7 @@ const Search = () => {
     console.log(listings);
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(location.search);
+    const urlParams = new URLSearchParams(window.location.search);
     const searchTermFromUrl = urlParams.get("searchTerm");
     const typeFromUrl = urlParams.get("type");
     const parkingFromUrl = urlParams.get("parking");
@@ -220,10 +220,23 @@ const Search = () => {
           </button>
         </form>
       </div>
-      <div>
+      <div className="flex-1">
         <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
           Listing Results:
         </h1>
+        <div className="p-7 flex flex-wrap gap-4">
+            {!loading && listings.length === 0 && (
+                <p className="text-xl text-slate-700">No listings found!</p>
+            )}
+            {loading && (
+                <p className="text-xl text-slate-700 text-center w-full">Loading...</p>
+            )}
+            {!loading && 
+             listings && 
+             listings.map((listing) => (
+                <ListingItem key={listing._id} listing={listing}/>
+            ))}
+        </div>
       </div>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('@tailwindcss/line-clamp'),
+//     // ...
+//   ],
+// }
+
+
+
+// The above was the original code but "require" was no reconized in ES6 so I had to change it to "import" and it worked. I replaced the above code with the one below.
+
+import lineClamp from '@tailwindcss/line-clamp';
+
 export default {
   content: [
     "./index.html",
@@ -7,5 +27,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    lineClamp,
+    // ...
+  ],
+};
